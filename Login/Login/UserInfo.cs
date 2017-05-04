7061757a3988c8f6321af8700dcac4822e923010
mrevilla <mrevilla@nevada.unr.edu>
2017-05-04 11:37:48 -0700

    Documented Code

--- Login/Login/UserInfo.cs
+++ Login/Login/UserInfo.cs
@@ -15,6 +15,50 @@ using System.IO;
 using Login.Resources.layout;
 using Newtonsoft.Json;
 
+/***
+ * This activity is used after the user has logged in. Several buttons are shown
+ * that will take the user to different features/activities of the application.
+ * 
+ * OnCreate 
+ * The function that is called after the Activity is created.
+ * 
+ * BtnEvents_Click  
+ * Event handler used when the user wants to go the events list page.
+ * 
+ * BtnEventInvite_Click 
+ * Event handler used when the user clicks on the event invite button.
+ * 
+ * BtnCreateEvent_Click 
+ * Event handler used when the user click on create events button
+ * 
+ * BtnMap_Click 
+ * Event handler used when the user clicks on the map button. The EventMap activity is
+ * started.
+ * 
+ * BtnLogout_Click
+ * Event handler that closes the activity when the user presses the log out button.
+ * 
+ * OnResume
+ * The function that is called after the activity visible to the user again.    
+ * 
+ * BtnEditProfile_Click 
+ * Event handler when the user clicks button to edit profile. Closes activity
+ * and goes EditProfile
+ * 
+ * BtnFriends_Click - 
+ * Event handler when the user clicks button to go to friends. Closes the activity
+ * and goes to Friends activity.
+ * 
+ * BtnLogout_Click 
+ * Event handler when the user wants to log out. Closes the activity.
+ * 
+ * MakeGetRequest    
+ * Sends GET Request to API
+ * 
+ * MakePostRequest    
+ * Sends HTTP Request to API
+ */
+
 namespace Login
 {
     [Activity(Label = "UserInfo")]
@@ -34,19 +78,24 @@ namespace Login
             // Create your application here
             SetContentView(Resource.Layout.UserInfo);
             tvUserInfo = (TextView)FindViewById(Resource.Id.tvUserInfo);
+            //Get user's token
             AccessToken = Intent.GetStringExtra("token");
+            //Close activity if the token receieved is null.
             if(AccessToken == null)
             {
                 Finish();
             }
             else
             {
+                //Obtain the database's ip for the userinfo controller.
                 string url = GetString(Resource.String.IP)+ "api/account/userinfo";
 
                 try
                 {
+                    //Get data from the url. 
                     string response = await MakeGetRequest(url);
 
+                    //Deserialize incoming data.
                     dynamic jsonData = JsonConvert.DeserializeObject(response);
                     firstName = jsonData.firstName;
                     lastName = jsonData.lastName;
@@ -62,7 +111,8 @@ namespace Login
                     throw;
                 }
             }
-
+            
+            //EVENT HANDLER
             Button btnEditProfile = (Button) FindViewById(Resource.Id.btnEditProfile);
             btnEditProfile.Click += BtnEditProfile_Click;
 
@@ -88,6 +138,7 @@ namespace Login
 
         private void BtnEvents_Click(object sender, EventArgs e)
         {
+            //Start EventList Activity with token and username extras
             Intent toEventList = new Intent(this,typeof(EventList));
             toEventList.PutExtra("token", AccessToken);
             toEventList.PutExtra("userName", userName);
@@ -96,6 +147,7 @@ namespace Login
 
         private void BtnEventInvite_Click(object sender, EventArgs e)
         {
+           //Start EventInvite Activity with token and username extras
             Intent toInvite = new Intent(this, typeof(EventInvite));
             toInvite.PutExtra("token", AccessToken);
             toInvite.PutExtra("userName", userName);
@@ -104,6 +156,7 @@ namespace Login
 
         private void BtnCreateEvent_Click(object sender, EventArgs e)
         {
+             //Start CreateEvent Activity with token extras
             Intent toCreateEvent = new Intent(this, typeof(CreateEvent));
             toCreateEvent.PutExtra("token", AccessToken);
             StartActivity(toCreateEvent);
@@ -111,6 +164,7 @@ namespace Login
 
         private void BtnMap_Click(object sender, EventArgs e)
         {
+            //Start EventMap Activity with token and username extras
             Intent toEventMap = new Intent(this, typeof(EventMap));
             toEventMap.PutExtra("token", AccessToken);
             toEventMap.PutExtra("userName", userName);
@@ -121,12 +175,16 @@ namespace Login
         {
             base.OnResume();
 
+            //Get database url
             string url = GetString(Resource.String.IP) + "api/account/userinfo";
 
             try
             {
+            
+                //get response from database using url
                 string response = await MakeGetRequest(url);
 
+                //deserialize data obtained.
                 dynamic jsonData = JsonConvert.DeserializeObject(response);
                 firstName = jsonData.firstName;
                 lastName = jsonData.lastName;
@@ -145,6 +203,7 @@ namespace Login
 
         private void BtnEditProfile_Click(object sender, EventArgs e)
         {
+              //Start Friends Activity with token and username extras
             Intent toEditProfile = new Intent(this,typeof(EditProfile));
             toEditProfile.PutExtra("AccessToken", AccessToken);
             StartActivity(toEditProfile);
@@ -154,6 +213,7 @@ namespace Login
 
         private void BtnFriends_Click(object sender, EventArgs e)
         {
+         //Start Friends Activity with token and username extras
             Intent activityUserInfo = new Intent(this, typeof(Friends));
             activityUserInfo.PutExtra("token", AccessToken);
             activityUserInfo.PutExtra("userName", userName);
@@ -167,15 +227,18 @@ namespace Login
 
         public static async Task<string> MakeGetRequest(string url)
         {
+            //create URL header.
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
             request.ContentType = "application/json; charset=utf-8";
             request.Method = "GET";
             request.Headers.Add("Authorization", "Bearer " + AccessToken);
 
+           //get response from the API
             var response = await request.GetResponseAsync();
             var respStream = response.GetResponseStream();
             respStream.Flush();
 
+            //read data
             using (StreamReader sr = new StreamReader(respStream))
             {
                 //Need to return this response 
@@ -187,16 +250,19 @@ namespace Login
 
         public async Task<string> MakePostRequest(string url, string serializedDataString, bool isJson)
         {
-            //simple request function 
+            //sets http request to be a post.
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
             if (isJson)
                 request.ContentType = "application/json";
             else
                 request.ContentType = "application/x-www-form-urlencoded";
 
+             //Add the token to the url
             request.Method = "POST";
             request.Headers.Add("Authorization", "Bearer " + AccessToken);
             var stream = await request.GetRequestStreamAsync();
+            
+            //write data            
             using (var writer = new StreamWriter(stream))
             {
                 writer.Write(serializedDataString);
@@ -204,6 +270,7 @@ namespace Login
                 writer.Dispose();
             }
 
+             //get data from API reply
             var response = await request.GetResponseAsync();
             var respStream = response.GetResponseStream();
 
