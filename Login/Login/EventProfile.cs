b4fbfd57da26ea541766e552c5b0b9dfa091dc03
jaimemoreno <jrjaimemoreno72@hotmail.com>
2017-05-05 14:13:17 -0700

    Documented code for EventProfile.cs

--- Login/Login/EventProfile.cs
+++ Login/Login/EventProfile.cs
@@ -15,12 +15,34 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Newtonsoft.Json;
+/***
+*EventProfile.cs Activity: 
+*
+* 
+* OnCreate: Initializes and sets activity view to display lists, buttons and textviews. 
+*
+* BtnETDelete_Click: Event handler which makes a delete request to delete event.
+*
+* BtnETSave_Click: Not implemented 
+*
+* MakeGetRequest: Receives data from web api through http get request and returns the content received. 
+* 
+* SetUpMap: Sets up the map view to display in activity.
+* 
+* OnMapReady: Handles all of the pin posting and markers sent to the map along with handlers for marker options
+* 
+* OnMapLongClick: not implemented.
+* 
+* MakeDeleteRequest: Sets the http delete request sent to the web api.
+*
+***/
 
 namespace Login
 {
     [Activity(Label = "EventProfile")]
     public class EventProfile : Activity, IOnMapReadyCallback, GoogleMap.IOnMapLongClickListener
     {
+        // initialize variables 
         private TextView tvEPError;
         private static string eventId;
         private static string AccessToken;
@@ -46,7 +68,7 @@ namespace Login
             SetContentView(Resource.Layout.EventProfile);
             
 
-            //set views
+            // set views
             tvEPError = (TextView) FindViewById(Resource.Id.tvEPError);
             etEPAddress1 = (EditText)FindViewById(Resource.Id.etEPAddress1);
             etEPAddress2 = (EditText)FindViewById(Resource.Id.etEPAddress2);
@@ -60,20 +82,21 @@ namespace Login
             etEPLatitude = (EditText) FindViewById(Resource.Id.etEPLatitude);
             etEPEventHost = (EditText) FindViewById(Resource.Id.etEPEventHost);
 
+            // wire up buttons 
             Button btnETSave = (Button)FindViewById(Resource.Id.btnETSave);
             btnETSave.Click += BtnETSave_Click;
             Button btnETDelete = (Button)FindViewById(Resource.Id.btnETDelete);
             btnETDelete.Click += BtnETDelete_Click;
 
-            //get the intents data
+            // get the intents data
             eventId = Intent.GetStringExtra("eventId");
             AccessToken = Intent.GetStringExtra("token");
             userName = Intent.GetStringExtra("userName");
 
-            //form url
+            // form url
             string url = GetString(Resource.String.IP) + "api/events/" + eventId;
 
-            //get the details about the event
+            // get the details about the event
             try
             {
                 string response = await MakeGetRequest(url);
@@ -112,11 +135,8 @@ namespace Login
                 etEPEventHost.Focusable = false;
                 etEPEventName.Focusable = false;
 
-                
+                // wire up buttons 
                 btnETSave.Visibility = ViewStates.Gone;
-                
-
-                
                 btnETDelete.Visibility = ViewStates.Gone;
                 
             }
@@ -126,10 +146,12 @@ namespace Login
 
         private async void BtnETDelete_Click(object sender, EventArgs e)
         {
+            // set url to delete event 
             string deleteUrl = GetString(Resource.String.IP) + "api/Events/" + eventId;
       
             try
             {
+                // make delete request to web api 
                 string response = await MakeDeleteRequest(deleteUrl, true);
                 Toast.MakeText(this, "Event Deleted", ToastLength.Short).Show();
                 Finish();
@@ -143,20 +165,23 @@ namespace Login
 
         private void BtnETSave_Click(object sender, EventArgs e)
         {
-            
+            // no implemented functionality 
         }
 
         public static async Task<string> MakeGetRequest(string url)
         {
+            //initialize http request and set method to a get request 
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
             request.ContentType = "application/json; charset=utf-8";
             request.Method = "GET";
             request.Headers.Add("Authorization", "Bearer " + AccessToken);
 
+            //initialize variables to stream data from web api
             var response = await request.GetResponseAsync();
             var respStream = response.GetResponseStream();
             respStream.Flush();
 
+            // read content that was returned from the get request
             using (StreamReader sr = new StreamReader(respStream))
             {
                 //Need to return this response 
@@ -168,6 +193,7 @@ namespace Login
 
         private void SetUpMap()
         {
+            // if there is no map set fragmet to display map specified in OnMapReady
             if (epMap == null)
             {
                 FragmentManager.FindFragmentById<MapFragment>(Resource.Id.epMap).GetMapAsync(this);
@@ -176,13 +202,12 @@ namespace Login
 
         public void OnMapReady(GoogleMap googleMap)
         {
-            epMap = googleMap;
-            
-
-      
+            // initialize map and set camera to lattitude location 
+            epMap = googleMap;     
             LatLng latlng = new LatLng(requestedEvent.Latitude, requestedEvent.Longitude);
             CameraUpdate camera = CameraUpdateFactory.NewLatLngZoom(latlng, 15);
             epMap.MoveCamera(camera);
+
             //place a pin where the event is located
             MarkerOptions options = new MarkerOptions().SetPosition(latlng).
                 SetTitle(requestedEvent.Name).SetSnippet(requestedEvent.Details);
@@ -198,18 +223,19 @@ namespace Login
 
         public void OnMapLongClick(LatLng point)
         {
-            
+            // not implemented 
         }
 
         public async Task<string> MakeDeleteRequest(string url, bool isJson)
         {
-            //simple request function 
+            // initialize http request and set url to passed string 
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
             if (isJson)
                 request.ContentType = "application/json";
             else
                 request.ContentType = "application/x-www-form-urlencoded";
 
+            // set http method to delete 
             request.Method = "DELETE";
             request.Headers.Add("Authorization", "Bearer " + AccessToken);
             var stream = await request.GetRequestStreamAsync();
@@ -220,6 +246,7 @@ namespace Login
                 writer.Dispose();
             }
 
+            // read content that was returned from the get request
             var response = await request.GetResponseAsync();
             var respStream = response.GetResponseStream();
 
